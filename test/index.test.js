bdfa087773a6617e15cc09531eeb6a57f08a2a65
jmarti <jmarti@theinit.com>
2017-03-26 17:17:47 +0200

    mover karma a un directorio test

---
+++ test/index.test.js
@@ -0,0 +1,17 @@
+/*
+'use strict';
+
+describe('myApp.index module', function() {
+
+    beforeEach(module('myApp.index'));
+
+    describe('index controller', function(){
+
+        it('should be defined', inject(function($controller) {
+            //spec body
+            var ctrl = $controller('IndexCtrl');
+            expect(ctrl).toBeDefined();
+        }));
+
+    });
+});*/
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -2,14 +2,14 @@
 module.exports = function(config) {
   config.set({
 
-    basePath: './',
+    basePath: '../',
 
     files: [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/components/**/*.js',
-      'app/view*/**/*.js',
+      'app/index/**/*.js',
+      'app/capitulo*/**/*.js',
       'test/**/*.js'
     ],
 
