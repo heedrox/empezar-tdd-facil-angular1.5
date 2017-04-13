fce13c766988164e358997eac288a3cf1bd08567
jmarti <jmarti@theinit.com>
2017-04-13 11:21:45 +0200

    añade primer test fallando

---
+++ test/capitulo4/capitulo4.controller.test.js
@@ -0,0 +1,8 @@
+describe("capitulo4 controller calcula los importes por cada producto y agrega un importe total", function() {
+
+    it("calcula el importe", function() {
+
+        expect(true).toBeFalsy();
+
+    });
+});
