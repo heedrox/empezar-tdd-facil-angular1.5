3e189b2b56275fda1f9b13b156d5237d66a48e5e
jmarti <jmarti@theinit.com>
2017-04-13 12:38:55 +0200

    implementa el calculo importes final

--- app/capitulo4/calculo-importes.js
+++ app/capitulo4/calculo-importes.js
@@ -4,5 +4,36 @@
 
     function CalculoImportes() {
 
+        return {
+            calcularImportes: calcularImportes
+        };
+
+        function calcularImportes(entrada) {
+            var entradaConImportes = entrada.map(aniadirImporte);
+            var entradaConImportesImpuestos = entradaConImportes.map(aniadirImporteImpuestos);
+            entradaConImportesImpuestos.push(calcularRegistroTotal(entradaConImportesImpuestos));
+            return entradaConImportesImpuestos;
+
+            function aniadirImporte(producto) {
+                producto.importe = producto.ctd * producto.pvp;
+                return producto;
+            }
+
+            function aniadirImporteImpuestos(producto) {
+                producto.importeImp = producto.importe * 1.21;
+                return producto;
+            }
+
+            function calcularRegistroTotal(productos) {
+                var sumaProductos = productos.reduce(suma, { importe: 0, importeImp: 0 });
+                return { id: 0, importe: sumaProductos.importe, importeImp: sumaProductos.importeImp };
+
+                function suma(a, b) {
+                    return { importe: a.importe + b.importe, importeImp: a.importeImp + b.importeImp };
+                }
+            }
+
+        }
+
     }
 })();
