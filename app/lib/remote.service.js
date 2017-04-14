43f35d7c7e06e88b12a994be3aa63ec848bd26ec
jmarti <jmarti@theinit.com>
2017-04-14 13:24:13 +0200

    añade extractData a remoteService

--- app/lib/remote.service.js
+++ app/lib/remote.service.js
@@ -3,12 +3,19 @@
         .service("RemoteService", [ '$http', RemoteService ] );
 
     function RemoteService($http) {
+
+        var ENDPOINT = "http://localhost:8888";
+
         return {
             post: post
         };
 
         function post(url, params) {
-            return $http.post(url, params);
+            return $http.post(ENDPOINT+url, params).then(extractData);
+
+            function extractData(x) {
+                return x.data;
+            }
         }
     }
 })();
