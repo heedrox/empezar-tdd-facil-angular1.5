3dbea33ed8a547c49e43a61a5a958007ce5e9ad5
jmarti <jmarti@theinit.com>
2017-04-14 12:53:01 +0200

    termina los tests con llamadas ajax

--- app/capitulo6/capitulo6.module.js
+++ app/capitulo6/capitulo6.module.js
@@ -1,16 +1,48 @@
 'use strict';
 
 angular.module('myApp.capitulo6', ['ngRoute', 'myApp.lib'])
-    .config(['$routeProvider', function($routeProvider) {
+    .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/capitulo6', {
             templateUrl: 'capitulo6/capitulo6.html',
             controller: 'Capitulo6Ctrl'
         });
     }])
 
-    .controller('Capitulo6Ctrl', ['$scope', 'RemoteService', function($scope, RemoteService) {
+    .controller('Capitulo6Ctrl', ['$scope', 'RemoteService', function ($scope, RemoteService) {
 
-        $scope.enviarFormulario = function() {
-            RemoteService.post();
+        $scope.enviarFormulario = function () {
+            var parametros = construirParametros();
+            RemoteService.post("/api/capitulo6", parametros);
+        };
+
+        function construirParametros() {
+            return {
+                email: $scope.form.email,
+                dateBirth: transformarFechaNacimiento($scope.form.fechaNacimiento),
+                package: transformarPaquete($scope.form.paquete),
+                monthPayment: obtenerPayment($scope.form.paquete, $scope.form.numMensualidades)
+            }
         }
+
+        function transformarFechaNacimiento(fecha) {
+            return fecha.getFullYear() + "" + zpad(fecha.getMonth() + 1) + zpad(fecha.getDate());
+
+            function zpad(d) {
+                return (d < 10) ? ('0' + d) : d;
+            }
+        }
+
+        function transformarPaquete(paquete) {
+            return paquete === "BASICO" ? 1 : 2;
+        }
+
+        function obtenerPayment(paquete, numMensualidades) {
+            var COSTES = {
+                "BASICO": 50,
+                "AVANZADO": 200
+            };
+
+            return COSTES[paquete] / numMensualidades;
+        }
+
     }]);
--- test/capitulo6/capitulo6.controller.test.js
+++ test/capitulo6/capitulo6.controller.test.js
@@ -12,14 +12,82 @@ describe("Capitulo6Ctrl hace llamadas AJAX a api/capitulo6 con los datos transfo
     beforeEach(function() {
         $scope = $rootScope.$new();
         $controller("Capitulo6Ctrl", { $scope: $scope });
+        $scope.form = {};
+        $scope.form.fechaNacimiento = new Date();
     });
 
-    it("llama al servidor cuando se pulsa enviar", function() {
+    it("llama al servidor con la api api/capitulo6", function() {
         spyOn(RemoteService, "post");
 
         $scope.enviarFormulario();
 
-        expect(RemoteService.post).toHaveBeenCalled();
+        expect(RemoteService.post.calls.first().args[0]).toBe("/api/capitulo6");
     });
+
+    it("envía el email", function() {
+        spyOn(RemoteService, "post");
+        $scope.form.email = "unemail@cualquiera.com";
+
+        $scope.enviarFormulario();
+
+        expect(RemoteService.post.calls.first().args[1].email).toBe("unemail@cualquiera.com");
+    });
+
+    var fechas = [
+        { fechaNacimiento: new Date(1977,03,7), dateBirth: "19770407" },
+        { fechaNacimiento: new Date(1977,10,10), dateBirth: "19771110" }
+    ];
+
+    using(fechas, function(caso) {
+        it("envía la fecha de nacimiento como dateBirth en formato YYYYMMDD para el caso "+caso.dateBirth, function() {
+            spyOn(RemoteService, "post");
+            $scope.form.fechaNacimiento = caso.fechaNacimiento;
+
+            $scope.enviarFormulario();
+
+            expect(RemoteService.post.calls.first().args[1].dateBirth).toBe(caso.dateBirth);
+        });
+
+    });
+
+    var paquetes = [
+        { paquete: "BASICO", package: 1 },
+        { paquete: "AVANZADO", package: 2 }
+    ];
+
+    using(paquetes, function(caso) {
+        it("envía el paquete traducido por 1 o 2 - "+caso.package, function() {
+            spyOn(RemoteService, "post");
+            $scope.form.paquete = caso.paquete;
+
+            $scope.enviarFormulario();
+
+            expect(RemoteService.post.calls.first().args[1].package).toBe(caso.package);
+        });
+
+    });
+
+    var COSTES = { BASICO: 50, AVANZADO: 200 };
+
+    var mensualidades = [
+        { paquete: "BASICO", numMensualidades: "6", monthPayment: COSTES.BASICO/6 },
+        { paquete: "BASICO", numMensualidades: "12", monthPayment: COSTES.BASICO/12 },
+        { paquete: "AVANZADO", numMensualidades: "10", monthPayment: COSTES.AVANZADO/10 },
+        { paquete: "AVANZADO", numMensualidades: "2", monthPayment: COSTES.AVANZADO/2 }
+    ];
+
+    using(mensualidades, function(caso) {
+        it("envía el pago mensual (monthPayment) - "+caso.monthPayment, function() {
+            spyOn(RemoteService, "post");
+            $scope.form.paquete = caso.paquete;
+            $scope.form.numMensualidades = caso.numMensualidades;
+
+            $scope.enviarFormulario();
+
+            expect(RemoteService.post.calls.first().args[1].monthPayment).toBe(caso.monthPayment);
+        });
+
+    });
+
 
 });
